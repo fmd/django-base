1151d230d955fb70b45897930819c7e95d938548
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-23 16:43:48 +0000

    fixed urls

--- djbase/urls.py
+++ djbase/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', include('frontend.urls')),
+    
+    # Frontend
+    url(r'^$', include('djbase.apps.frontend.urls')),
 
+    # Admin 
     url(r'^admin/', include(admin.site.urls)),
 )
